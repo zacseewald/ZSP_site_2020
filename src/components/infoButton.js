55b564a40c542ea4e8c725eb8d0e7a46536ad672
Zac Seewald <zaxblackdragon.com>
2020-01-08 12:38:14 -0600

    info btn

---
+++ src/components/infoButton.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class infoButton extends Component {
+    render() {
+        return (
+            <div className="info-button-parent-box" >
+                <div className="into-button-child-border">
+                    <p className="info-button-text" >i</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default infoButton
