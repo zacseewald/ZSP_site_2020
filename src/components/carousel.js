479f714769926cd533f5c5445fa533a8089fe8bd
Zac Seewald <zaxblackdragon.com>
2020-01-14 23:19:29 -0600

    need to fix the dble loading of images on the carousel at loading

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -13,14 +13,16 @@ import HousesImageFive from './utilities/images/homes/homesFive.jpg';
 export class Carousel extends Component {
 
   state = { 
-  // array of images for the houses carousel
- 
-  imageGallery: this.props.location.pathname.split('/').pop(),
 
-  // imageLeft: ImgInfo.this.props.location.pathname.split('/').pop()[4],
-  // imageCenter: ImgInfo.this.props.location.pathname.split('/').pop()[0],
-  // image: ImgInfo.this.props.location.pathname.split('/').pop()[1],
-  
+  imageGallery: this.props.location.pathname.split('/').pop(),
+  imagePathLeft: '',
+  imagePathCenter: '',
+  imagePathRight: '',
+
+  carouselImageLeft: ImgInfo[this.props.location.pathname.split('/').pop()][4]["image_fileName"],
+  carouselImageCenter: ImgInfo[this.props.location.pathname.split('/').pop()][0]["image_fileName"],
+  carouselImageRight: ImgInfo[this.props.location.pathname.split('/').pop()][1]["image_fileName"],
+
   HousesImage: [ HousesImageOne, HousesImageTwo, HousesImageThree, HousesImageFour, HousesImageFive], 
 
 
@@ -32,73 +34,110 @@ export class Carousel extends Component {
 handleClickLeft = (e) => {
 e.preventDefault();
 
+const imageGalleryLength = ImgInfo[this.state.imageGallery].length - 1;
+const gallery = this.state.imageGallery;
+const pathLeft = ImgInfo[this.state.imageGallery][this.state.targetPositionLeft]["image_fileName"]; 
+const pathCenter = ImgInfo[this.state.imageGallery][this.state.targetPositionCenter]["image_fileName"];
+const pathRight = ImgInfo[this.state.imageGallery][this.state.targetPositionRight]["image_fileName"];  
+
 if (this.state.targetPositionLeft === 0 ) {
     this.setState ({
-      targetPositionLeft: 4,
+      targetPositionLeft: imageGalleryLength,
       targetPositionCenter: 0,
       targetPositionRight: 1,
+
+     
+      carouselImageLeft: ImgInfo[this.state.imageGallery][imageGalleryLength]["image_fileName"],
+      carouselImageCenter: ImgInfo[this.state.imageGallery][0]["image_fileName"],
+      carouselImageRight: ImgInfo[this.state.imageGallery][1]["image_fileName"],
+
+      imagePathLeft: require("./utilities/images/" + gallery + "/"+ pathLeft),
+      imagePathCenter: require("./utilities/images/" + gallery + "/"+ pathCenter),
+      imagePathRight: require("./utilities/images/" + gallery + "/"+ pathRight),
+
     });
-  console.log("left " + this.state.targetPositionLeft);
-  console.log("center " + this.state.targetPositionCenter); 
-  console.log("right " + this.state.targetPositionRight);
-
-
   } else if (this.state.targetPositionRight === 0) {
     this.setState ({
       targetPositionLeft: 2,
       targetPositionCenter: 3,
-      targetPositionRight: 4,
+      targetPositionRight: imageGalleryLength,
+
+
+      
+      carouselImageLeft: ImgInfo[this.state.imageGallery][2]["image_fileName"],
+      carouselImageCenter: ImgInfo[this.state.imageGallery][3]["image_fileName"],
+      carouselImageRight: ImgInfo[this.state.imageGallery][imageGalleryLength]["image_fileName"],
+
+      imagePathLeft: require("./utilities/images/" + gallery + "/"+ pathLeft),
+      imagePathCenter: require("./utilities/images/" + gallery + "/"+ pathCenter),
+      imagePathRight: require("./utilities/images/" + gallery + "/"+ pathRight),
     });
   } 
-  
   else if (this.state.targetPositionCenter === 0) {
      this.setState({
         targetPositionLeft: 3,
-        targetPositionCenter: 4,
+        targetPositionCenter: imageGalleryLength,
         targetPositionRight: 0,
-      });
-  console.log("left " + this.state.targetPositionLeft);
-  console.log("center " + this.state.targetPositionCenter); 
-  console.log("right " + this.state.targetPositionRight);
-  
-  }
 
+        // imageGallery: this.props.location.pathname.split('/').pop(),
+        carouselImageLeft: ImgInfo[this.state.imageGallery][3]["image_fileName"],
+        carouselImageCenter: ImgInfo[this.state.imageGallery][imageGalleryLength]["image_fileName"],
+        carouselImageRight: ImgInfo[this.state.imageGallery][0]["image_fileName"],
+
+        imagePathLeft: require("./utilities/images/" + gallery + "/"+ pathLeft),
+        imagePathCenter: require("./utilities/images/" + gallery + "/"+ pathCenter),
+        imagePathRight: require("./utilities/images/" + gallery + "/"+ pathRight),
+      });
+  }
   else {
     this.setState ({
-       targetPositionLeft: this.state.targetPositionLeft - 1,
+        targetPositionLeft: this.state.targetPositionLeft - 1,
         targetPositionCenter: this.state.targetPositionCenter - 1,
         targetPositionRight: this.state.targetPositionRight - 1,
+
+        // imageGallery: this.props.location.pathname.split('/').pop(),
+        carouselImageLeft: ImgInfo[this.state.imageGallery][this.state.targetPositionLeft - 1]["image_fileName"],
+        carouselImageCenter: ImgInfo[this.state.imageGallery][this.state.targetPositionCenter - 1]["image_fileName"],
+        carouselImageRight: ImgInfo[this.state.imageGallery][this.state.targetPositionRight - 1]["image_fileName"],
+
+        imagePathLeft: require("./utilities/images/" + gallery + "/"+ pathLeft),
+        imagePathCenter: require("./utilities/images/" + gallery + "/"+ pathCenter),
+        imagePathRight: require("./utilities/images/" + gallery + "/"+ pathRight),
+
     });
   }
-
-
-
-
 }
 
 handleClickRight = (e) => {
 e.preventDefault();
 console.log("right");
 console.log(this.state.targetPositionRight);
-
 }
+componentDidMount() {
+  // const gallery = this.state.imageGallery;
+  // const pathLeft = ImgInfo[this.state.imageGallery][this.state.targetPositionLeft]["image_fileName"]; 
+  // const pathCenter = ImgInfo[this.state.imageGallery][this.state.targetPositionCenter]["image_fileName"];
+  // const pathRight = ImgInfo[this.state.imageGallery][this.state.targetPositionRight]["image_fileName"];  
 
-
+    this.setState( { 
+      imagePathLeft: require("./utilities/images/" + this.state.imageGallery + "/"+ ImgInfo[this.state.imageGallery][this.state.targetPositionLeft]["image_fileName"]),
+      imagePathCenter: require("./utilities/images/" + this.state.imageGallery + "/"+ ImgInfo[this.state.imageGallery][this.state.targetPositionCenter]["image_fileName"]),
+      imagePathRight: require("./utilities/images/" + this.state.imageGallery + "/"+ ImgInfo[this.state.imageGallery][this.state.targetPositionRight]["image_fileName"]),
+    }); 
+}
   render() {
-    console.log(this.state.imageGallery)
-//set the state for the images here with a function.
 
     return (
       
       <div className="carousel-box">
-          <img className="left-img" src={this.state.HousesImage[this.state.targetPositionLeft]} />
+          <img className="left-img" src={this.state.carouselImageLeft} />
           <button className="btn-left" onClick={this.handleClickLeft} ></button>
 
-          <img className="visible-img" src={this.state.HousesImage[this.state.targetPositionCenter]} />
+          <img className="visible-img" src={this.state.imagePathCenter} />
 
           <button className="btn-right" onClick={this.handleClickRight} ></button>
           
-          <img className="right-img" src={this.state.HousesImage[this.state.targetPositionRight]} />
+          <img className="right-img" src={this.state.carouselImageRight} />
 
       </div>
     )
