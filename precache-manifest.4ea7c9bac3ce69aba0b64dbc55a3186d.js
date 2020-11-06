184fb3891e60724c79a7626067227da31c338998
Zac Seewald <zaxblackdragon.com>
2020-11-06 11:11:59 -0600

    Updates

---
+++ precache-manifest.4ea7c9bac3ce69aba0b64dbc55a3186d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "be9c740bb3bb96e7e268daa9e6da0f01",
+    "url": "/ZSP_site_2020/static/media/twitter.be9c740b.png"
+  },
+  {
+    "revision": "ce5aae11ecfa556a83cc74d392cf10ec",
+    "url": "/ZSP_site_2020/static/media/linkedIn.ce5aae11.png"
+  },
+  {
+    "revision": "f44462c67ff0dcc24c1e126da46ea1a9",
+    "url": "/ZSP_site_2020/static/media/instagram.f44462c6.png"
+  },
+  {
+    "revision": "cdff8e3d08c825538e462deb79f4938b",
+    "url": "/ZSP_site_2020/static/media/email.cdff8e3d.png"
+  },
+  {
+    "revision": "eedc1a5b9c7f8c545141867d20040e0c",
+    "url": "/ZSP_site_2020/static/media/about.eedc1a5b.jpg"
+  },
+  {
+    "revision": "45ae5b70277f08ef121f",
+    "url": "/ZSP_site_2020/static/js/runtime~main.13835a7a.js"
+  },
+  {
+    "revision": "171c61320b764b2e687a",
+    "url": "/ZSP_site_2020/static/js/main.2a34d1d2.chunk.js"
+  },
+  {
+    "revision": "55f1b78ab86412b830f3",
+    "url": "/ZSP_site_2020/static/js/2.6941da78.chunk.js"
+  },
+  {
+    "revision": "171c61320b764b2e687a",
+    "url": "/ZSP_site_2020/static/css/main.69596b4a.chunk.css"
+  },
+  {
+    "revision": "cb403d9679f2eddf163f72dcbf707b9d",
+    "url": "/ZSP_site_2020/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ZSP_site_2020/precache-manifest.4ea7c9bac3ce69aba0b64dbc55a3186d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ZSP_site_2020/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
