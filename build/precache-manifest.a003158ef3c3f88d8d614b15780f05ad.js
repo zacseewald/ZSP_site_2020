b39a0656e18fe4b7d28316e5dc0d989d75232c47
Zac Seewald <zaxblackdragon.com>
2020-11-06 14:24:52 -0600

    attempting to fix detached HEAD

---
+++ build/precache-manifest.a003158ef3c3f88d8d614b15780f05ad.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "be9c740bb3bb96e7e268daa9e6da0f01",
+    "url": "/static/media/twitter.be9c740b.png"
+  },
+  {
+    "revision": "ce5aae11ecfa556a83cc74d392cf10ec",
+    "url": "/static/media/linkedIn.ce5aae11.png"
+  },
+  {
+    "revision": "f44462c67ff0dcc24c1e126da46ea1a9",
+    "url": "/static/media/instagram.f44462c6.png"
+  },
+  {
+    "revision": "cdff8e3d08c825538e462deb79f4938b",
+    "url": "/static/media/email.cdff8e3d.png"
+  },
+  {
+    "revision": "eedc1a5b9c7f8c545141867d20040e0c",
+    "url": "/static/media/about.eedc1a5b.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e4efcdf9d885f6f2c5c7",
+    "url": "/static/js/main.e2afad2f.chunk.js"
+  },
+  {
+    "revision": "55f1b78ab86412b830f3",
+    "url": "/static/js/2.6941da78.chunk.js"
+  },
+  {
+    "revision": "e4efcdf9d885f6f2c5c7",
+    "url": "/static/css/main.69596b4a.chunk.css"
+  },
+  {
+    "revision": "af862637cca9305160dbf9cff39d34e9",
+    "url": "/index.html"
+  }
+];
