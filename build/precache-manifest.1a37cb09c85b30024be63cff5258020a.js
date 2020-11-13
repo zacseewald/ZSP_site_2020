a4aa7792e9595cc9766591e4cc9e07938f0fddd8
Zac Seewald <zaxblackdragon.com>
2020-11-13 09:01:19 -0600

    removed build from the .gitignore

---
+++ build/precache-manifest.1a37cb09c85b30024be63cff5258020a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "72f01c9e838d43cb4b49",
+    "url": "./static/css/main.69596b4a.chunk.css"
+  },
+  {
+    "revision": "72f01c9e838d43cb4b49",
+    "url": "./static/js/main.149eafb1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "55f1b78ab86412b830f3",
+    "url": "./static/js/2.6941da78.chunk.js"
+  },
+  {
+    "revision": "f44462c67ff0dcc24c1e126da46ea1a9",
+    "url": "./static/media/instagram.f44462c6.png"
+  },
+  {
+    "revision": "ce5aae11ecfa556a83cc74d392cf10ec",
+    "url": "./static/media/linkedIn.ce5aae11.png"
+  },
+  {
+    "revision": "be9c740bb3bb96e7e268daa9e6da0f01",
+    "url": "./static/media/twitter.be9c740b.png"
+  },
+  {
+    "revision": "eedc1a5b9c7f8c545141867d20040e0c",
+    "url": "./static/media/about.eedc1a5b.jpg"
+  },
+  {
+    "revision": "cdff8e3d08c825538e462deb79f4938b",
+    "url": "./static/media/email.cdff8e3d.png"
+  },
+  {
+    "revision": "3091ba9e8b9abf60519b0d24687fe6c2",
+    "url": "./index.html"
+  }
+];
